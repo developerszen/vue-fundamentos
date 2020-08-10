881fed86b82381e8f77a85395dd23aef060c9753
Rodrigo Caetano <didiercreando@gmail.com>
2020-08-10 12:01:18 -0400

    Clas01 Interpolaciones

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <v-app>
+    <v-container>
+      
+      <class-interpolations></class-interpolations>
+
+    </v-container>
+  </v-app>
+</template>
+
+<script>
+import ClassInterpolations from "@/components/01-interpolations.vue";
+
+export default {
+  components: {
+    ClassInterpolations
+  }
+}
+</script>
---
+++ src/components/01-interpolations.vue
@@ -0,0 +1,21 @@
+<template>
+    <v-row>
+        <v-col class="primary secondary--text font-weight-black" cols="12">
+            Interpolaciones
+        </v-col>
+
+        <v-col cols="12">
+            {{ name }}
+        </v-col>
+    </v-row>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            name: "Harina"
+        }
+    }
+}
+</script>
